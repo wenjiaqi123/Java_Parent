3c98c6251ec6b417580e840885222d0f5d7870bb
闻家奇 <939949243@qq.com>
2020-03-20 17:02:17 +0800

    文件

--- java_base_file/src/main/java/com/gsm/controller/FileController.java
+++ java_base_file/src/main/java/com/gsm/controller/FileController.java
@@ -14,8 +14,21 @@ public class FileController {
     private FileService fileService;
 
     @PostMapping("/file")
-    public String insertFile(MultipartFile file){
+    public String insertFile(MultipartFile file) {
+        System.out.println(1);
         String s = fileService.insertFile(file);
+        System.out.println(2);
         return s;
     }
+    /**
+     * SpringBoot   必须会 熟练会，像写 for 循环一样熟练
+     *      配置信息能够看懂
+     *
+     *  基础  CRUD ，码农
+     *      SSM 框架（虽然不用了，但是沿用讲法）
+     *      Struts2  +  Spring   + Hibernate    SSH（明清时期）
+     *      SpringMVC  +  Spring   + Mybatis    SSM (近现代)   2015-2016年左右
+     *      SpringBoot      当代
+     *      SpringCloud     当代
+     */
 }
