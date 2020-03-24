048b2be550cb159e5bf1ea38b8c885de303f2fa0
闻家奇 <939949243@qq.com>
2020-03-24 18:54:11 +0800

    项目调整

---
+++ aaaaa_redis/src/main/java/demo/Demo.java
@@ -0,0 +1,9 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import utils.RedisUtils;
+
+public class Demo {
+    @Autowired
+    private RedisUtils redisUtils;
+}
--- java_base_file/src/main/java/com/gsm/service/impl/FileServiceImpl.java
+++ java_base_file/src/main/java/com/gsm/service/impl/FileServiceImpl.java
@@ -7,9 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 @Service
 public class FileServiceImpl implements FileService {
     @Autowired
